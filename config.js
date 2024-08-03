d0604430428d29fcf8130e093f716b9cd1f9f19b
karim-w-png <derdouchkarim3@gmail.com>
2024-08-03 09:29:39 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Sharef and Snowhite",
+    introTitle: 'Snowwhite',
+    introDesc: `f had l3alam kaml emr bghatni wla bghit shi bnt bhalk bla manzid nhdar rak 3arfa kolshi`,
+    btnIntro: 'HiHi',
+    title: 'wash katbghini afdwa bhal kif kanbghik 🥰',
+    desc: 'had lwa9t li dwzna kaml wash bdlt shi haja f hyatk wla la  ',
+    btnYes: 'ah love u <33',
+    btnNo: 'la :3',
+    question: 'kanbghiiik bzaf ahbiba w kantwa7shek kola d9i9a daba la kan momkin bghit nshofk <3',
+    btnReply: ' send to u <3',
+    reply: 'kanmout 3lk w daba ansiftlk vedio tshofni ahbiba wlah ',
+    mess: 'nt 3arf alhob maghtrdihash lia f wjhi kanbghiiik ',
+    messDesc: 'lyoma blil ahbib tana ansiftlk bzaf vedios w tswr hta t9n3i mni.',
+    btnAccept: 'wakha hbiba <3',
+    messLink: 'https://karim-w-png.github.io/Fadwa-this-for-u/' //your message link. For example: https://www.facebook.com/messages/t/100014188333536
+}
